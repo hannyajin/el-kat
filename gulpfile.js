8d8353d2e885ab3d3ebad178e6c90b6fd862969d
Hannyajin <talmo.christian@gmail.com>
2014-10-31 04:21:13 +0200

    gulp tasks added

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var watch = require('gulp-watch');
+var livereload = require('gulp-livereload');
+
+// compile less to css
+gulp.task('less', function() {
+  return gulp.src('./public/styles/main.less')
+          .pipe(less())
+          .pipe(gulp.dest('./public/styles/main.css'))
+          .pipe(livereload());
+});
+
+// watch changes to less files
+gulp.task('watch', function() {
+  gulp.watch('./public/styles/*.less', ['less']);
+});
+
+// watch by default
+gulp.task('default', ['watch']);
